fdb8e25cae3d21f7181d0f093e5435c13c84abdd
Олег Негрозов <genromgsu@gmail.com>
2018-01-13 21:25:06 +0300

    After review. Fix filldb to using exist data for insert

--- create.sql
+++ create.sql
@@ -2,25 +2,30 @@ CREATE DATABASE hhschool;
 
 CREATE SCHEMA genro;
 
-CREATE TYPE genro.FIO AS (
+SET SEARCH_PATH = genro;
+
+CREATE TYPE FIO AS (
     first_name  TEXT,
     second_name TEXT,
     patronymic  TEXT
 );
 
-CREATE TABLE genro.users (
+CREATE TABLE users (
     user_id    SERIAL PRIMARY KEY, -- id пользователя
     email      VARCHAR(254) NOT NULL, -- email
     pwd        TEXT         NOT NULL, -- пароль (хеш офкорс)
     time_reg   TIMESTAMP    NOT NULL, -- время регистрации
     time_login TIMESTAMP, -- время последнего захода на сайт
-    name       genro.FIO, -- фио для резюме
+    name       FIO, -- фио для резюме
     birthday   DATE -- дата рождения для вычисления возраста
 );
 
-CREATE TABLE genro.vacancies (
-    vac_id       SERIAL PRIMARY KEY, -- id вакансии
-    company_name TEXT      NOT NULL, -- id компании, оттуда ее название
+CREATE UNIQUE INDEX email_index
+    ON users (email);
+
+CREATE TABLE vacancies (
+    vacancy_id   SERIAL PRIMARY KEY, -- id вакансии
+    company_name TEXT      NOT NULL, -- название компании
     lang         REGCONFIG NOT NULL, -- язык вакансии, для конфигурации полнотекстового поиска
     position     TEXT      NOT NULL, -- название должности
     description  TEXT, -- описание вакансии
@@ -32,30 +37,37 @@ CREATE TABLE genro.vacancies (
     time_delete  TIMESTAMP -- время окончания публиикации вакансии
 );
 
-CREATE INDEX vacancies_idx
-    ON genro.vacancies USING GIN (to_tsvector(lang, position || ' ' || description));
+CREATE INDEX vacancies_index
+    ON vacancies USING GIN (to_tsvector(lang, position || ' ' || description));
 
 CREATE INDEX experience_index
-    ON genro.vacancies (experience); -- частый поиск с применением операций сравнения, для чего B-деревья и нужны
+    ON vacancies (experience); -- частый поиск с применением операций сравнения, для чего B-деревья и нужны
 
-CREATE TABLE genro.summaries (
+CREATE INDEX salary_index
+    ON vacancies (salary_min, salary_max); -- чаще все таки ищут по минимальной зарплате, но можно и по обоим границам
+
+
+CREATE TABLE summaries (
     summary_id SERIAL PRIMARY KEY, -- id резюме
-    user_id    INT REFERENCES genro.users, -- создатель резюме, оттуда брать ФИО, возраст
+    user_id    INT REFERENCES users, -- создатель резюме, оттуда брать ФИО, возраст
     salary_min INT, -- минимальная з/п
     salary_max INT, -- максимальная з/п
-    expirience INT, -- опыт работы в мес.
+    experience INT, -- опыт работы в мес.
     skills     TEXT ARRAY -- список скилов
 );
 
-CREATE TABLE genro.responses (
+CREATE INDEX user_experience_index
+    ON summaries (experience); -- частый поиск с применением операций сравнения, для чего B-деревья и нужны
+
+CREATE TABLE responses (
     response_id SERIAL PRIMARY KEY, -- id отклика
-    vacancy_id  INT REFERENCES genro.vacancies, -- id компании
-    summary_id  INT REFERENCES genro.summaries -- id резюме
+    vacancy_id  INT REFERENCES vacancies, -- id компании
+    summary_id  INT REFERENCES summaries -- id резюме
 );
 
-CREATE TABLE genro.messages (
+CREATE TABLE messages (
     messsage_id SERIAL PRIMARY KEY, -- id message
-    response_id INT REFERENCES genro.responses, -- id отклика, в рамках которого это сообщения отправлено
+    response_id INT REFERENCES responses, -- id отклика, в рамках которого это сообщения отправлено
     forward     BOOLEAN   NOT NULL DEFAULT TRUE, -- флаг отправителя (true - работодатель, false - соискатель)
     msg         TEXT      NOT NULL, -- сам текст сообщения
     distime     TIMESTAMP NOT NULL -- время отправки сообщения
--- filldb.sql
+++ filldb.sql
@@ -1,14 +1,16 @@
-INSERT INTO genro.users (email, pwd, time_reg, time_login, name, birthday)
+SET SEARCH_PATH = genro;
+
+INSERT INTO users (email, pwd, time_reg, time_login, name, birthday)
     SELECT
         concat(left(md5(cast(random() AS TEXT)), 200), '@gmail.com'),
         md5(cast(random() AS TEXT)),
         '2017-01-01 12:00:00' :: TIMESTAMP + random() * '365 days' :: INTERVAL,
         '2017-01-01 12:00:00' :: TIMESTAMP + random() * '365 days' :: INTERVAL,
-        (md5(cast(random() AS TEXT)), md5(cast(random() AS TEXT)), 'ИОСИФОВИЧ') :: genro.FIO,
+        (md5(cast(random() AS TEXT)), md5(cast(random() AS TEXT)), 'ИОСИФОВИЧ') :: FIO,
         '1990-01-01 12:00:00' :: TIMESTAMP + random() * '3650 days' :: INTERVAL
     FROM generate_series(1, 5000) AS t(num);
 
-INSERT INTO genro.vacancies (company_name, lang, position, description, salary_min, salary_max, experience, skills, time_create, time_delete)
+INSERT INTO vacancies (company_name, lang, position, description, salary_min, salary_max, experience, skills, time_create, time_delete)
     SELECT
         md5(cast(random() AS TEXT)),
         'english',
@@ -24,29 +26,40 @@ INSERT INTO genro.vacancies (company_name, lang, position, description, salary_m
         '2017-01-01 12:00:00' :: TIMESTAMP + random() * '365 days' :: INTERVAL
     FROM generate_series(1, 1000000) AS t(num);
 
-INSERT INTO genro.summaries (user_id, salary_min, salary_max, expirience, skills)
+-- на каждого пользователя в среднем 200 резюмю (но разное количество)
+INSERT INTO summaries (user_id, salary_min, salary_max, experience, skills)
     SELECT
-        round(random() * 4999 + 1),
+        u.user_id,
         round(random() * 400000),
-        -- а юзеры хотят много
         round(random() * 400000),
         round(random() * 6),
-        -- а опыта нет
-        ARRAY ['С', 'С++', 'C+++', '1C', '2C', '2C++'] -- а эту шутку ваши коллеги придумали
-    FROM generate_series(1, 1000000) AS t(num);
+        ARRAY ['С', 'С++', 'C+++', '1C', '2C', '2C++']
+    FROM users u
+        LEFT JOIN generate_series(1, 2000) AS t(num) ON (1 = round(random() * 10));
+-- 1001602 rows affected in 16s 239ms
 
--- 500к откликов, на каждыый в среднем 4 сообщения, итого 2кк сообщений
-
-INSERT INTO genro.responses (vacancy_id, summary_id)
+-- ~500k откликов.
+INSERT INTO responses (vacancy_id, summary_id)
     SELECT
-        round(random() * 999999 + 1),
-        round(random() * 999999 + 1)
-    FROM generate_series(1, 500000) AS t(num);
+        v.vacancy_id,
+        s.summary_id
+    FROM (SELECT vacancy_id
+          FROM vacancies
+          ORDER BY random()
+          LIMIT 5000) AS v
+        JOIN (SELECT summary_id
+              FROM summaries
+              ORDER BY random()
+              LIMIT 5000) AS s ON 1 = round(random() * 50);
+-- 499399 rows affected in 15s 454ms
 
-INSERT INTO genro.messages (response_id, forward, msg, distime)
+-- на каждый отклик в среднем 4 сообщения
+INSERT INTO messages (response_id, forward, msg, distime)
     SELECT
-        round(random() * 499999 + 1),
+        r.response_id,
         cast(cast(random() AS INT) AS BOOLEAN),
         md5(cast(random() AS TEXT)),
         '2017-01-01 12:00:00' :: TIMESTAMP + random() * '365 days' :: INTERVAL
-    FROM generate_series(1, 2000000) AS t(num);
+    FROM (SELECT response_id
+          FROM responses) AS r LEFT JOIN generate_series(1, 20) AS t(num) ON 1 = round(random() * 5);
+-- 2003849 rows affected in 30s 683ms
